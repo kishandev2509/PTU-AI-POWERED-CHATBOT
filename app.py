4f54249dc16a003670e2d904bb124781142423a2
Vanshika <vv10072005@gmail.com>
2025-08-16 15:40:56 +0530

    app.py

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ from chatbot.ptu_utils import PTUUtils
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+with app.app_context():
+    db.create_all()
 app.config['SECRET_KEY'] = 'your-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "student_portal.db")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
